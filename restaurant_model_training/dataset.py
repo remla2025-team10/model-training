760daa0100a5278a00d142990ac1df2dfa40f802
Haoran Tang <h.tang-6@student.tudelft.nl>
2025-05-26 10:37:37 +0200

    fix: fix pipeline and refactor code

--- restaurant_model_training/dataset.py
+++ restaurant_model_training/dataset.py
@@ -5,5 +5,5 @@ def get_data(raw_data_path, processed_data_path):
     dataset = pd.read_csv(raw_data_path, delimiter='\t', quoting=3)
     corpus = preprocess_dataframe(dataset, output_path=str(processed_data_path))['Processed_Review']
     labels = dataset.loc[:, 'Liked'].values
-    
+
     return corpus, labels
